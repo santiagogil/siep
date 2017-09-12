80c7a0a4bd6c5fa0af7d4631f283cd76b68a1938
MaTiUs77 <matius77@gmail.com>
2017-09-12 14:56:36 -0300

    Datatable optimizado
    El controlados se encuentra optimizado solo para la consulta como modo superadmin.
    
    Resta optimizar en modo admin y usuario

--- Controller/MatriculasController.php
+++ Controller/MatriculasController.php
@@ -1,51 +1,63 @@
 <?php
 App::uses('AppController', 'Controller');
 
-class MatriculasController extends AppController {
+class MatriculasController extends AppController
+{
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
 
-	public function beforeFilter() {
-		parent::beforeFilter();
-		/* ACCESOS SEGÚN ROLES DE USUARIOS (INICIO).
-		*Si el usuario tiene un rol de superadmin le damos acceso a todo. Si no es así (se trata de un usuario "admin o usuario") tendrá acceso sólo a las acciones que les correspondan.
-		*/
-		if (($this->Auth->user('role') === 'superadmin')  || ($this->Auth->user('role') === 'admin')) {
-			$this->Auth->allow();
-		} elseif ($this->Auth->user('role') === 'usuario') {
-			$this->Auth->allow('index', 'view');
-		}
-		/* FIN */
-	}
+        /* ACCESOS SEGÚN ROLES DE USUARIOS (INICIO).
+        *Si el usuario tiene un rol de superadmin le damos acceso a todo. Si no es así (se trata de un usuario "admin o usuario") tendrá acceso sólo a las acciones que les correspondan.
+        */
+        switch ($this->Auth->user('role')) {
+            case 'superadmin':
+            case 'admin':
+                $this->Auth->allow();
+            break;
+            case 'usuario':
+                $this->Auth->allow('index', 'view');
+            break;
+        }
+    }
 
-	function index() {
+    public function index()
+    {
+    }
 
-	}
+    public function requestDatatable()
+    {
+        $this->loadModel('Curso');
 
-	function requestDatatable() {
+        $userCentroId = $this->getUserCentroId();
+        $userRole = $this->Auth->user('role');
 
-		$this->loadModel('Curso');
-		$this->Curso->contain();
-		
-		$userCentroId = $this->getUserCentroId();
-		$userRole = $this->Auth->user('role');
-		if ($userRole === 'admin') {
-			$result = $this->Curso->find('all', array('conditions'=>array('Curso.centro_id' => $userCentroId)));
-		} else if ($userRole === 'usuario') {
-			$nivelCentro = $this->Curso->Centro->find('list', array('fields'=>array('nivel_servicio'), 'conditions'=>array('id'=>$userCentroId)));
-			$nivelCentroId = $this->Curso->Centro->find('list', array('fields'=>array('id'), 'conditions'=>array('nivel_servicio'=>$nivelCentro))); 		
-			$result = $this->Curso->find('all', array('conditions'=>array('Curso.centro_id' => $nivelCentroId)));
-		} else if ($userRole === 'superadmin') {
-			$result = $this->Curso->Centro->find('all');
-		}	
+        // Modifique las rutas de ROLES al formato SWITCH que es mas llevadero que los IF
+        switch ($userRole) {
+            case 'admin':
+                $result = $this->Curso->find('all', array('conditions'=>array('Curso.centro_id' => $userCentroId)));
+            break;
+            case 'usuario':
+                $nivelCentro = $this->Curso->Centro->find('list', array('fields'=>array('nivel_servicio'), 'conditions'=>array('id'=>$userCentroId)));
+                $nivelCentroId = $this->Curso->Centro->find('list', array('fields'=>array('id'), 'conditions'=>array('nivel_servicio'=>$nivelCentro)));
+                $result = $this->Curso->find('all', array('conditions'=>array('Curso.centro_id' => $nivelCentroId)));
+            break;
+            case 'superadmin':
+	              // Al definir contain solo relaciona el modelo solicitado en el array y no todas las dependencias definidas en el modelo
+	              $result = $this->Curso->find('all', array(
+	                   'contain' => array('Centro')
+	              ));
+            break;
+        }
 
-		$result = [
-			"data" => $result
-		];
+        $result = [
+            "data" => $result
+        ];
 
-		$this->autoRender = false;
-		$this->response->type('json');
+        $this->autoRender = false;
+        $this->response->type('json');
 
-		$json = json_encode($result);
-		$this->response->body($json);
-	}
+        $json = json_encode($result);
+        $this->response->body($json);
+    }
 }
-?>
